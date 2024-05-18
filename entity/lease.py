90babc021c8444900e296d69c26b17ef983af364
amisha.india <kumariamisha2022@gmail.com>
2024-05-18 23:12:23 +0530

    modified for better readability

--- entity/lease.py
+++ entity/lease.py
@@ -1,18 +1,18 @@
 #Lease model to manage the deatil
 class Lease:
-    def __init__(self,leaseID ,vehicleId,customerID,startDate,endDate,type):
-        self.__leaseId = leaseID
-        self.__vehicleId = vehicleId
-        self.__customerID = customerID
-        self.__startDate = startDate
-        self.__endDate = endDate
+    def __init__(self,lease_id ,vehicle_id,customer_id,start_date,end_date,type):
+        self.__lease_id = lease_id
+        self.__vehicle_id = vehicle_id
+        self.__customer_id = customer_id
+        self.__start_date = start_date
+        self.__end_date = end_date
         self.__type = type
 
     def get_lease_id(self):
-        return self.__leaseId
+        return self.__lease_id
     
-    def set_lease_id(self, leaseID):
-        self.__leaseId = leaseID
+    def set_lease_id(self, lease_id):
+        self.__lease_id = lease_id
 
     def get_lease(self):
-        return {"leaseID":self.__leaseId,"vehicleID": self.__vehicleId,"customerID": self.__customerID,"startDate": self.__startDate,"endDate": self.__endDate,"Type": self.__type}
+        return {"lease_id":self.__lease_id,"vehicle_id": self.__vehicle_id,"customer_id": self.__customer_id,"start_date": self.__start_date,"end_date": self.__end_date,"Type": self.__type}
